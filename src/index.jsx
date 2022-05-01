e1c8d28621ec616f41f74ce1d98d62e1eae6acd5
yong.teng <tengyong@buession.com>
2022-05-02 00:08:31 +0800

    修改主页介绍 && 添加统计代码

--- src/index.jsx
+++ src/index.jsx
@@ -7,10 +7,12 @@ banner:
     - { name: 'Github >', href: 'https://github.com/buession/buession-prototype' }
   caption: '当前版本: v2.0.0'
 features: 
-  - { name: '优雅', desc: '经过精雕细琢，我们带给大家一个精心设计的、拥有卓越的视觉与交互体验的文档构建工具' }
-  - { name: '灵动', desc: '我们拥有非常灵活的 插件机制 与 主题定制 功能，正在努力构建活跃的插件社区。也许初次使用未见其惊艳，但当你灵活使用插件后便会发现她的强大' }
-  - { name: '简洁', desc: '以 ‘无形’ 代替 ‘有形’，从开发体验到用户界面的呈现，不断去除冗余的设计，使用户专注于写作与阅读' }
-  - { name: '开源', desc: '作为开源项目，我们拥有高质量的代码、完善的自动化测试流程，对社区的需求能够作出积极快速响应' }
+  - { name: '优雅', desc: '旨在提供便捷的、可靠的基于原生 JavaScript/TypeScript 扩展的类库，拥有完善的文档和高可读性的源码' }
+  - { name: '易用', desc: '觉大部分 API 基于原生 JavaScript/TypeScript 对象/类的扩展，参考学习 prototype.js' }
+  - { name: '简洁', desc: '与其它框架无耦合，高内聚低耦合' }
+  - { name: '高效', desc: 'min 版本 js 文件仅 15.1 kB，加载速度快，代码简洁运行效率高' }
+  - { name: '兼容', desc: '支持 Node 和 Browser 运行环境，支持主流浏览器，支持原生 JS 和 ES' }
+  - { name: '开源', desc: '作为开源项目，我们拥有高质量的代码，对社区的需求能够作出积极快速响应' }
 
 footer:
   copyright:
