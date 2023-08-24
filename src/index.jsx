3bcab527703bfe52c33bf64bbe064df5c2369b73
yong.teng <yong.teng@liangvi.com>
2023-08-24 13:10:09 +0800

    更新最新版本号

--- src/index.jsx
+++ src/index.jsx
@@ -5,12 +5,12 @@ banner:
   btns: 
     - { name: '开 始', href: '/docs/quickstart.html', primary: true }
     - { name: 'Github >', href: 'https://github.com/buession/buession-prototype' }
-  caption: '当前版本: v2.2.1'
+  caption: '当前版本: v2.2.2'
 features: 
   - { name: '优雅', desc: '旨在提供便捷的、可靠的基于原生 JavaScript/TypeScript 扩展的类库，拥有完善的文档和高可读性的源码' }
   - { name: '易用', desc: '觉大部分 API 基于原生 JavaScript/TypeScript 对象/类的扩展，参考学习 prototype.js' }
   - { name: '简洁', desc: '与其它框架无耦合，高内聚低耦合' }
-  - { name: '高效', desc: 'min 版本 js 文件仅 15.1 kB，加载速度快，代码简洁运行效率高' }
+  - { name: '高效', desc: 'min 版本 js 文件仅 16 kB，加载速度快，代码简洁运行效率高' }
   - { name: '兼容', desc: '支持 Node 和 Browser 运行环境，支持主流浏览器，支持原生 JS 和 ES' }
   - { name: '开源', desc: '作为开源项目，我们拥有高质量的代码，对社区的需求能够作出积极快速响应' }
 
@@ -25,6 +25,7 @@ footer:
     后端开源库:
       - { name: 'Buession Framework', href: 'https://www.buession.com/' }
       - { name: 'Buession Security', href: 'https://security.buession.com/' }
+      - { name: 'Buession Logging', href: 'https://logging.buession.com/' }
       - { name: 'Buession SpringBoot', href: 'https://springboot.buession.com/' }
       - { name: 'Buession SpringCloud', href: 'https://springcloud.buession.com/' }
       - { name: 'Buession Cas', href: 'https://cas.buession.com/' }
